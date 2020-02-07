48ecc7df039383c7880be422c038a6d4d745753d
zenobit <kvvark@gmail.com>
2020-02-07 06:12:23 +0100

    Update VOIDinstaller.sh

--- VOIDinstaller.sh
+++ VOIDinstaller.sh
@@ -1,2 +1,20 @@
 #!/bin/bash
 # Void Linux post-install script
+
+void-repo-nonfree
+void-repo-multilib
+void-repo-multilib-nonfree
+nvidia
+nvidia-libs-32bit
+nano
+xtools
+git
+firefox
+steam
+libdrm-32bit
+alsa-utils
+alsa-tools
+sox
+mpv
+smplayer
+dmenu
