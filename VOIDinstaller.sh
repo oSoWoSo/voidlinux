86b5782b79df31266e71a2a014542bc55f66d5b4
zenobit <kvvark@gmail.com>
2020-02-07 06:20:47 +0100

    Update VOIDinstaller.sh

--- VOIDinstaller.sh
+++ VOIDinstaller.sh
@@ -1,20 +1,69 @@
 #!/bin/bash
 # Void Linux post-install script
 
-void-repo-nonfree
-void-repo-multilib
-void-repo-multilib-nonfree
-nvidia
-nvidia-libs-32bit
-nano
-xtools
-git
-firefox
-steam
-libdrm-32bit
-alsa-utils
-alsa-tools
-sox
-mpv
-smplayer
-dmenu
+basic=(
+  void-repo-nonfree
+  void-repo-multilib
+  void-repo-multilib-nonfree
+  wget
+  cmake
+  acpi
+  tlp
+  emacs-x11
+  redshift
+  nvidia
+  nvidia-libs-32bit
+  nano
+  xtools
+  git
+  firefox
+  steam
+  libdrm-32bit
+  alsa-utils
+  #alsa-tools
+  sox
+  mpv
+  smplayer
+  dmenu
+)
+
+echo 'installing basics...'
+sudo xbps-install ${basic[@]}
+echo 'xorg'
+x=(
+    xf86-video-intel    # intel driver (needed for screen brightness)
+    xf86-input-evdev    # drivers for keyboard, trackpad, etc
+    xinit
+    xbacklight
+    xterm
+    xclip
+    xorg-fonts
+    xsetroot            # for dwm status bar
+    setxkbmap           # so I can set keyboard to colemak in X
+)
+
+echo 'suckless dependencies'
+sl=(
+    libX11-devel
+    libXft-devel
+    libXinerama-devel
+    libXrandr-devel
+)
+
+
+
+echo 'compile sucklees stuff'
+make=(
+    st
+    dwm
+    slock
+    surf
+    sxiv
+)
+
+for util in ${make[@]}
+do
+    git clone https://github.com/AxelBohm/$util.git /home/xel/src/$util
+    cd /home/xel/src/$util
+    sudo make clean install
+done
